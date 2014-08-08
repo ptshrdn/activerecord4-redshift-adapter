50f47ceff23e9c75195a520834f6787b587d07d1
ptshrdn <psheridan@gmail.com>
2014-08-08 07:12:47 -0400

    Mark supports disable referential integrity as false

--- lib/active_record/connection_adapters/redshift/referential_integrity.rb
+++ lib/active_record/connection_adapters/redshift/referential_integrity.rb
@@ -3,26 +3,7 @@ module ActiveRecord
     class RedshiftAdapter < AbstractAdapter
       module ReferentialIntegrity
         def supports_disable_referential_integrity? #:nodoc:
-          true
-        end
-
-        def disable_referential_integrity #:nodoc:
-          if supports_disable_referential_integrity?
-            begin
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL" }.join(";"))
-            rescue
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER" }.join(";"))
-            end
-          end
-          yield
-        ensure
-          if supports_disable_referential_integrity?
-            begin
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL" }.join(";"))
-            rescue
-              execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER" }.join(";"))
-            end
-          end
+          false
         end
       end
     end
